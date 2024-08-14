16a7d8181f5bb956af611e41bcf84720a7686912
Arun-jose-2003 <arunjosesince2003@gmail.com>
2024-08-15 02:02:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [price, SetPrice] = useState(0);
+  const [discount, SetDiscound] = useState(0);
+  const [Discoundrate, SetDiscoundrate] = useState(0)
+  const [amount, SetAmount] = useState(0)
+  function Calculate() {
+    const Discoundrate = (price / 100) * discount;
+    SetDiscoundrate(Math.round(Discoundrate) + Discoundrate)
+    SetAmount(price - Discoundrate)
+  }
+
+  return (
+    <>
+      <div className="container ">
+      
+      <div className="head" >
+        <h1 >Real-time discount calculator</h1>
+        <p style={{color:'#1a2a6c'}}>Use the discount calculator online to find out the exact amount you will save!</p>
+      </div>
+
+      <div className="row">
+        <div className="input col-6">
+
+          <label htmlFor="">Enter The Amount</label><br /><br />
+          <input type="text" onChange={(e) => SetPrice(e.target.value)} placeholder='Enter The Amount' /><br /><br />
+          <label htmlFor="">Discound %</label><br /><br />
+          <input type="text" onChange={(e) => SetDiscound(e.target.value)} placeholder='Discound %' /><br /><br />
+          <button className='btn' onClick={Calculate}>Calculate</button><br />
+
+        </div>
+        <div className="output col-6">
+          
+            <h1 >Amount paid</h1>
+           
+            <div className="price">
+              <h2>New price</h2>
+              <h1>&#8377; {amount}</h1>
+            </div>
+          
+          <div className="discount">
+
+            <p>Amound Saved: &#8377;{Discoundrate}</p>
+
+            <p>Actual Price: &#8377;{price}</p>
+
+
+          </div>
+
+
+
+        </div>
+      </div>
+
+
+      </div>
+
+
+
+
+
+
+
+
+    
+    </>
+  )
+}
+
+export default App
